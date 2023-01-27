aab08f4d930dc79043630d5693d535443753dfde
Adrian Ibarra <adrianibarra1222@gmail.com>
2023-01-27 15:37:05 -0800

    Create PeakIndexInAMoutainArray.py

---
+++ PeakIndexInAMoutainArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+        while l < r:
+            midPoint = (l + r) // 2
+            print(arr[midPoint])
+            if arr[midPoint] > arr[midPoint - 1] and arr[midPoint] > arr[midPoint + 1]:
+                return midPoint
+            if arr[midPoint] > arr[midPoint - 1]:
+                l += 1
+            if arr[midPoint] > arr[midPoint + 1]:
+                r -= 1
+        return -1
+            
